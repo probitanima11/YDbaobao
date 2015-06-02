d32949eeb40a792579ab61fe7c9e7bad617961c0
hwiba <sungcheon.lee@outlook.com>
2015-06-02 16:58:30 +0900

    [참고] thymeleaf form 예제의 컨트롤러

--- src/com/ydbaobao/controller/TestController.java
+++ src/com/ydbaobao/controller/TestController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ydbaobao.model.TestThymeleaf;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -19,5 +22,17 @@ public class TestController {
 		list.add("3");
 		model.addAttribute("list", list);
 		return "thymeleaf";
+	}
+	
+	@RequestMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("TestThymeleaf", new TestThymeleaf());
+		return "thymeform";
+	}
+	
+	@RequestMapping(value="", method = RequestMethod.POST)
+	public void test (TestThymeleaf tt) {
+		System.out.println(tt.getId());
+		System.out.println(tt.getName());
 	}
 }
