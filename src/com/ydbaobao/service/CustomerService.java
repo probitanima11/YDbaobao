7da8379f2177475b907c329e9b57b31ad48e6627
hwiba <sungcheon.lee@outlook.com>
2015-06-02 15:41:22 +0900

    thymeleaf 한글 설정 및 index.html 적

---
+++ src/com/ydbaobao/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.ydbaobao.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.ydbaobao.dao.CustomerDao;
+import com.ydbaobao.model.Customer;
+
+@Service
+public class CustomerService {
+	@Resource
+	private CustomerDao customerDao;
+
+	public void join(Customer customer) {
+		customerDao.createCustomer(customer);
+	}
+
+}
