fce78b7bed9082c14573a950a2b9ea827de30c83
hwiba <sungcheon.lee@outlook.com>
2015-06-03 16:47:11 +0900

    ProductDao readListByCategory 구현

--- src/com/ydbaobao/controller/HomeController.java
+++ src/com/ydbaobao/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.ydbaobao.controller;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import com.ydbaobao.service.CategoryService;
 
 @Controller
 public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Resource
 	private CategoryService categorySevice; 
--- src/com/ydbaobao/dao/ProductsDao.java
+++ src/com/ydbaobao/dao/ProductsDao.java
@@ -9,6 +9,8 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import com.ydbaobao.model.Brand;
+import com.ydbaobao.model.Category;
 import com.ydbaobao.model.Product;
 
 @Repository
@@ -23,7 +25,14 @@ public class ProductsDao extends JdbcDaoSupport {
 	}
 	
 	public List<Product> readListByCategory(int categoryId) {
-		String sql = "";
-		return null;
+		String sql = "select * from PRODUCTS where categoryId=?";
+		return getJdbcTemplate().query(
+				sql, (rs, rowNum) -> new Product(
+						rs.getInt("productId"), rs.getString("productName"),
+						new Category(rs.getInt("categoryId"), null),
+						new Brand(rs.getInt("brandId"), null),
+						rs.getInt("productPrice"), rs.getString("productImage"),
+						rs.getString("productDescription"), rs.getLong("productCreateDate"),
+						rs.getLong("productUpdateDate")), categoryId);
 	}
 }
