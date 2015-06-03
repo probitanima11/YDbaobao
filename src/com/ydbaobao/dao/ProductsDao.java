cdf38f9f65d455a688c67f0c4998b487fb870375
hwiba <sungcheon.lee@outlook.com>
2015-06-03 16:12:30 +0900

    _brand.jsp, ProductsService 생성. 카테고리뷰에서 제품 리스트, 브랜드 리스트를 볼 수 있도록.

--- src/com/ydbaobao/dao/ProductsDao.java
+++ src/com/ydbaobao/dao/ProductsDao.java
@@ -22,18 +22,8 @@ public class ProductsDao extends JdbcDaoSupport {
 		setDataSource(dataSource);
 	}
 	
-//	public List<PComment> readListByPId(String pId, String noteId) {
-//		String sql = "select * from PCOMMENTS, USERS where PCOMMENTS.userId = USERS.userId AND pId = ? AND noteId = ?";
-//		List<PComment> pc = getJdbcTemplate().query(sql, (rs, rowNum) -> new PComment(rs.getString("pCommentId"), rs.getString("pId"),
-//						rs.getString("sameSenCount"), rs.getString("sameSenIndex"), rs.getString("pCommentText"),
-//						rs.getString("selectedText"), rs.getString("pCommentCreateDate"),
-//						new SessionUser(rs.getString("userId"), rs.getString("userName"), rs.getString("userImage")),
-//						new Note(rs.getString("noteId"))), pId, noteId);
-//		return pc;
-//	}
-	
-	public List<Product> readListByCategory(String categoryId) {
-		
+	public List<Product> readListByCategory(int categoryId) {
+		String sql = "";
 		return null;
 	}
 }
