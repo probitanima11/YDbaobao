0e22b4af5856fa2b0ec7eb8e3a78564941b1697d
최성원 <lavie.choi@gmail.com>
2015-06-03 15:06:55 +0900

    itemDao read method 구현.

--- src/com/ydbaobao/dao/ItemDao.java
+++ src/com/ydbaobao/dao/ItemDao.java
@@ -6,9 +6,13 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
+import com.ydbaobao.model.Customer;
 import com.ydbaobao.model.Item;
+import com.ydbaobao.model.Order;
+import com.ydbaobao.model.Product;
 
 public class ItemDao extends JdbcDaoSupport {
 	@Resource
@@ -20,7 +24,19 @@ public class ItemDao extends JdbcDaoSupport {
 	}
 
 	public List<Item> read(String customerId) {
-		return null;
+		String sql = "select * from ITEMS where customerId=?";
+		try {
+			return getJdbcTemplate().query(sql, (rs, rowNum) -> new Item(
+					rs.getInt("itemId"),
+					new Customer(rs.getString("customerId")),
+					new Product(rs.getInt("productId")),
+					new Order(rs.getInt("orderId")),
+					rs.getString("size"),
+					rs.getInt("quantity")
+					), customerId);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
 	}
 	
 	public void delete() {
