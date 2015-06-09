535bc68a2beecc06ad445042fa90c08c0234e0e8
giyatto <bluem31@gmail.com>
2015-06-09 13:06:43 +0900

    [수정] DB의 Products_Stocks 테이블 삭제.

--- src/com/ydbaobao/dao/StockDao.java
+++ src/com/ydbaobao/dao/StockDao.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import com.ydbaobao.model.Product;
 import com.ydbaobao.model.Stock;
 
 @Repository
@@ -25,7 +26,7 @@ public class StockDao extends JdbcDaoSupport {
 		String sql = "select * from STOCKS where productId=?";
 		return getJdbcTemplate().query(
 				sql, (rs, rowNum) -> new Stock(
-						rs.getInt("stockId"), 
+						rs.getInt("stockId"), new Product(rs.getInt("productId")),
 						rs.getString("size"), rs.getInt("quantity")), productId);
 	}
 }
---
+++ src/main/resources/db/migration/V3__Delete_PRODUCTS_STOCKS_MAP_table.sql
@@ -0,0 +1,6 @@
+DROP TABLE PRODUCTS_STOCKS;
+
+ALTER TABLE STOCKS ADD productId int NOT NULL after stockId;
+SET foreign_key_checks = 0;
+ALTER TABLE STOCKS ADD FOREIGN KEY (productId) REFERENCES PRODUCTS(productId) ON DELETE CASCADE;
+SET foreign_key_checks = 1;
