7ac468a0a856bcb3b954d3b7378ed64daefa9574
최성원 <lavie.choi@gmail.com>
2015-06-16 22:12:25 +0900

    [추가] 승인전까지 로그인페이지만 보이도록 #86

--- src/com/ydbaobao/intercepter/GradeCheckIntercepter.java
+++ src/com/ydbaobao/intercepter/GradeCheckIntercepter.java
@@ -1,7 +1,30 @@
 package com.ydbaobao.intercepter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.support.ServletRequestUtil;
+
 public class GradeCheckIntercepter extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		if (!ServletRequestUtil.hasAuthorizationFromCustomer(session)) {
+			response.sendRedirect("/");
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		super.postHandle(request, response, handler, modelAndView);
+	}
 
 }
