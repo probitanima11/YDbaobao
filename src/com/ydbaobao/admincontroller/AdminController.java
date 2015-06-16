3de4099bee201e71fc9d6f06acfd568da15d3fde
das <das@192.168.1.79>
2015-06-17 00:20:43 +0900

    [수정] 첫화면관리기능 - 사진 추가 기능

--- src/com/ydbaobao/admincontroller/AdminController.java
+++ src/com/ydbaobao/admincontroller/AdminController.java
@@ -1,17 +1,19 @@
 package com.ydbaobao.admincontroller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ydbaobao.dao.GradeDao;
 import com.ydbaobao.model.AdminConfig;
@@ -25,7 +27,6 @@ import com.ydbaobao.service.ProductService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 	private static String password = "1111";
 
 	@Resource
@@ -40,10 +41,10 @@ public class AdminController {
 	private AdminConfigService adminConfigService;
 	@Resource
 	private GradeDao gradeDao;
-	
+
 	/**
-	 * 관리자 페이지 접근을 위한 GET 요청을 응답.
-	 * session 체크를 하여 sessionAdmin이 Null일 경우 관리자 번호 체크 페이지로 포워딩.
+	 * 관리자 페이지 접근을 위한 GET 요청을 응답. session 체크를 하여 sessionAdmin이 Null일 경우 관리자 번호
+	 * 체크 페이지로 포워딩.
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String home(HttpSession session) {
@@ -57,17 +58,17 @@ public class AdminController {
 	public String check() {
 		return "admin/adminCheck";
 	}
-	
 
 	@RequestMapping(value = "/check", method = RequestMethod.POST)
-	public String checkForm(@RequestParam String adminPassword, HttpSession session) {
+	public String checkForm(@RequestParam String adminPassword,
+			HttpSession session) {
 		if (adminPassword.equals(password)) {
 			session.setAttribute("sessionAdmin", adminPassword);
 			return "admin/admin";
 		}
 		return "admin/adminCheck";
 	}
-	
+
 	@RequestMapping("/logout")
 	public String logout(HttpSession session) {
 		if (null == session.getAttribute("sessionAdmin")) {
@@ -76,7 +77,7 @@ public class AdminController {
 		session.removeAttribute("sessionAdmin");
 		return "redirect:/";
 	}
-	
+
 	@RequestMapping(value = "/manage/grade", method = RequestMethod.GET)
 	public String manageGrade(Model model) {
 		model.addAttribute("grades", gradeDao.readGrades());
@@ -87,7 +88,8 @@ public class AdminController {
 	public String addProduct(Model model) {
 		model.addAttribute("product", new Product());
 		model.addAttribute("brandList", brandService.readBrands());
-		model.addAttribute("unregisteredProductsCountByBrand", productService.unregisteredProductsCountByBrand());
+		model.addAttribute("unregisteredProductsCountByBrand",
+				productService.unregisteredProductsCountByBrand());
 		return "admin/productRegistration";
 	}
 
@@ -96,15 +98,70 @@ public class AdminController {
 		model.addAttribute("adminConfig", adminConfigService.read());
 		return "admin/config";
 	}
-	
+
 	@RequestMapping(value = "/manage/order", method = RequestMethod.GET)
 	public String manageOrder() {
 		return "admin/orderManager";
 	}
-	
+
 	@RequestMapping(value = "/config/update", method = RequestMethod.PUT)
-	public String configUpdate(Model model, @RequestParam AdminConfig adminConfig) {
-		model.addAttribute("adminConfig", adminConfigService.update(adminConfig));
+	public String configUpdate(Model model,
+			@RequestParam AdminConfig adminConfig) {
+		model.addAttribute("adminConfig",
+				adminConfigService.update(adminConfig));
 		return "/admin/config";
+	}
+
+	/**
+	 * 첫화면 이미지 설정 관리 페이지 
+	 * 이미지 경로와 저장된 이미지 갯수를 돌려준다.
+	 */
+	@RequestMapping(value = "/indexImages")
+	public String indexImage(Model model) {
+		List<String> imgPath = new ArrayList<String>();
+		String defaultPath = "/img/index/default.png";
+		int imgCount = 0;
+		for (int i = 0; i < 8; i++) {
+			String filePath = "index_0" + i + ".jpg";
+			File f = new File("/home/baobao/index/"+filePath);
+			if (f.isFile()) {
+				imgPath.add("/img/index/"+filePath);
+				imgCount++;
+			} else {
+				imgPath.add(defaultPath);
+			}
+		}
+		model.addAttribute("imgPath", imgPath);
+		model.addAttribute("imgCount", imgCount);
+		return "/admin/indexManager";
+	}
+	
+	/**
+	 * 첫화면 이미지 설정 관리 페이지 
+	 * 이미지 추가.
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 */
+	@RequestMapping(value = "/indexImages", method = RequestMethod.POST)
+	public String indexImageCreate(Model model, @RequestParam MultipartFile profileImage, @RequestParam int imgCount) throws IllegalStateException, IOException {
+		profileImage.transferTo(new File("/home/baobao/index/index_0" + imgCount + ".jpg"));
+		
+		List<String> imgPath = new ArrayList<String>();
+		String defaultPath = "/img/index/default.png";
+		imgCount = 0;
+		for (int i = 0; i < 8; i++) {
+			String filePath = "index_0" + i + ".jpg";
+			File f = new File("/home/baobao/index/"+filePath);
+			if (f.isFile()) {
+				imgPath.add("/img/index/"+filePath);
+				imgCount++;
+			} else {
+				imgPath.add(defaultPath);
+			}
+		}
+		model.addAttribute("imgPath", imgPath);
+		model.addAttribute("imgCount", imgCount);
+		
+		return "/admin/indexManager";
 	}
 }
