e3a377a633d01da489c9842c3e1848d67e6bf8d2
최성원 <lavie.choi@gmail.com>
2015-06-16 19:22:35 +0900

    [추가] 승인전까지 로그인페이지만 보이도록 #86 xml 설정

---
+++ src/com/ydbaobao/intercepter/GradeCheckIntercepter.java
@@ -0,0 +1,7 @@
+package com.ydbaobao.intercepter;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class GradeCheckIntercepter extends HandlerInterceptorAdapter {
+
+}
