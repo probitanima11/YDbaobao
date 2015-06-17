40c58a0c9ac6e55130bcd576d1d5118c70512cba
Youngbin Jang <jyb0823@gmail.com>
2015-06-17 15:57:08 +0900

    [추가] 카테고리 별 브랜드 네비게이션 적용 #101

--- src/com/ydbaobao/service/ProductService.java
+++ src/com/ydbaobao/service/ProductService.java
@@ -3,8 +3,6 @@ package com.ydbaobao.service;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import javax.annotation.Resource;
 
@@ -161,5 +159,9 @@ public class ProductService {
 			return true;
 		}
 		return false;
+	}
+
+	public List<Product> readByCategoryIdAndBrandId(int categoryId, int brandId) {
+		return productDao.readByCategoryIdAndBrandId(categoryId, brandId);
 	}
 }
