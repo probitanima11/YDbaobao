641ea3460636fd1b64c9c96f541424d3b12c778a
Youngbin Jang <jyb0823@gmail.com>
2015-06-18 10:48:10 +0900

    [추가] ProductService에 Transaction처리 #114

--- src/com/ydbaobao/service/ProductService.java
+++ src/com/ydbaobao/service/ProductService.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.support.ImageResizeUtil;
@@ -20,6 +21,7 @@ import com.ydbaobao.model.Category;
 import com.ydbaobao.model.Product;
 
 @Service
+@Transactional
 public class ProductService {
 	private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
 	
