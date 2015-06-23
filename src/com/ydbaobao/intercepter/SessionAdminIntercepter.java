cd55e82025a421980406cdad5f505231ef9480db
das <das@192.168.1.79>
2015-06-23 14:17:05 +0900

    [수정] 권한부분 intercepter에서 처리

--- src/com/ydbaobao/intercepter/SessionAdminIntercepter.java
+++ src/com/ydbaobao/intercepter/SessionAdminIntercepter.java
@@ -3,12 +3,17 @@ package com.ydbaobao.intercepter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class SessionAdminIntercepter extends HandlerInterceptorAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(SessionAdminIntercepter.class);
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.debug("preHandle");
 		if(request.getSession().getAttribute("sessionAdmin") == null) {
 			response.sendRedirect("/");
 			return Boolean.FALSE;
